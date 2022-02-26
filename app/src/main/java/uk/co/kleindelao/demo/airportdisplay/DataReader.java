48d99a0b56accacc4bcf84f09a8e69e905339db9
Raimund Klein <770876+Chessray@users.noreply.github.com>
2022-02-26 07:51:55 +0000

    Presort flights directly after reading.

--- app/src/main/java/uk/co/kleindelao/demo/airportdisplay/DataReader.java
+++ app/src/main/java/uk/co/kleindelao/demo/airportdisplay/DataReader.java
@@ -2,27 +2,29 @@ package uk.co.kleindelao.demo.airportdisplay;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Comparator.comparing;
+import static org.apache.commons.csv.CSVFormat.DEFAULT;
+import static org.apache.commons.csv.CSVParser.parse;
 
 import com.google.common.annotations.VisibleForTesting;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
 
 public class DataReader {
   private final List<Flight> flights;
 
   public DataReader() throws IOException {
     final var converter = new CsvRecordToFlightConverter();
-    flights = CSVParser.parse(DataReader.class.getResource("/flights.csv"), UTF_8,
-                           CSVFormat.Builder.create(CSVFormat.DEFAULT)
-                                            .setHeader()
-                                            .setSkipHeaderRecord(true)
-                                            .build())
-                       .stream()
-                       .map(converter::convert)
-                       .toList();
+    flights = parse(DataReader.class.getResource("/flights.csv"), UTF_8,
+        CSVFormat.Builder.create(DEFAULT)
+                         .setHeader()
+                         .setSkipHeaderRecord(true)
+                         .build())
+        .stream()
+        .map(converter::convert)
+        .sorted(comparing(Flight::departureTime))
+        .toList();
   }
 
   @VisibleForTesting
@@ -30,16 +32,10 @@ public class DataReader {
     return flights.size();
   }
 
-  public Flight getFirstEntry() {
-    return flights.iterator().next();
-  }
-
   public List<Flight> getFlightsForDateSortedByTime(final LocalDate date) {
     final var dayOfWeek = date.getDayOfWeek();
     return flights.stream()
                   .filter(flight -> flight.fliesOnDayOfWeek(dayOfWeek))
-                  .sorted(comparing(Flight::departureTime))
                   .toList();
   }
-
 }
--- app/src/test/java/uk/co/kleindelao/demo/airportdisplay/DataReaderTest.java
+++ app/src/test/java/uk/co/kleindelao/demo/airportdisplay/DataReaderTest.java
@@ -1,11 +1,9 @@
 package uk.co.kleindelao.demo.airportdisplay;
 
-import static java.time.DayOfWeek.TUESDAY;
 import static org.assertj.core.api.BDDAssertions.then;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import org.junit.jupiter.api.Test;
 
 public class DataReaderTest {
@@ -19,17 +17,6 @@ public class DataReaderTest {
     final var numberOfEntries = underTest.getNumberOfEntries();
 
     then(numberOfEntries).isEqualTo(27);
-  }
-
-  @Test
-  void shouldReadFirstRecord() {
-    final var firstFlight = underTest.getFirstEntry();
-
-    then(firstFlight.departureTime()).isEqualTo(LocalTime.of(9, 0, 0));
-    then(firstFlight.destination()).isEqualTo("Antigua");
-    then(firstFlight.destinationIataCode()).isEqualTo("ANU");
-    then(firstFlight.number()).isEqualTo("VS033");
-    then(firstFlight.days()).containsExactly(TUESDAY);
   }
 
   @Test
